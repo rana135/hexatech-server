506651f618522c9259af0c70dfb546b8dd7923e5
rana135 <rana286090@gmail.com>
2022-07-29 18:29:31 +0600

    new task added

--- index.js
+++ index.js
@@ -14,7 +14,14 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
+        // Rana vai:-
         const ProductCollection = client.db("db-hexatech").collection("service");
+        // Emran vai:-
+        const PurchaseCollection = client.db("db-hexatech").collection("purchase");
+        // Sakil vai:-
+        const newTaskCollection = client.db("db-hexatech").collection('newTask');
+
+        
         // get All product:-
         app.get('/products', async (req, res) => {
             const query = {}
@@ -45,8 +52,31 @@ async function run() {
                 $set: data,
             }
             const result = await ProductCollection.updateOne(filter, updateDoc, options)
-            res.send({ result})
+            res.send({ result })
         })
+
+
+        // Emran vai:- purchase:-
+        app.post('/purchase', async (req, res) => {
+            const newPurchase = req.body
+            console.log(newPurchase);
+            const result = await PurchaseCollection.insertOne(newPurchase)
+            res.send(result)
+        })
+
+
+        // Sakil vai:-
+        app.post('/newTask', async (req, res) => {
+            const newTaskAdd = req.body;
+            const result = await newTaskCollection.insertOne(newTaskAdd)
+            res.send({ success: true, message: "added ", result });
+        })
+        app.get('/taskDetails', async (req, res) => {
+            const query = {}
+            const result = await newTaskCollection.find(query).toArray()
+            res.send(result)
+        });
+
     }
     finally {
         // await client.close();
