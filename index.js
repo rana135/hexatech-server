8eb9ff271134f6b2300c4c720ef4a88581ac321e
rana135 <rana286090@gmail.com>
2022-08-05 15:02:44 +0600

    allpurchase and alltask added

--- index.js
+++ index.js
@@ -16,12 +16,18 @@ async function run() {
     try {
         // Rana vai:-
         const ProductCollection = client.db("db-hexatech").collection("service");
+
         // Emran vai:-
         const PurchaseCollection = client.db("db-hexatech").collection("purchase");
+        const Allpurchase = client.db("db-hexatech").collection("purchasecollection");
+
         // Sakil vai:-
         const newTaskCollection = client.db("db-hexatech").collection('newTask');
+        const allTask = client.db("db-hexatech").collection("taskcollection");
 
-        
+
+
+
         // get All product:-
         app.get('/products', async (req, res) => {
             const query = {}
@@ -52,7 +58,7 @@ async function run() {
                 $set: data,
             }
             const result = await ProductCollection.updateOne(filter, updateDoc, options)
-            res.send({ result })
+            res.send(result)
         })
 
 
@@ -61,6 +67,31 @@ async function run() {
             const newPurchase = req.body
             console.log(newPurchase);
             const result = await PurchaseCollection.insertOne(newPurchase)
+            res.send(result)
+        })
+        app.post('/purchase', async (req, res) => {
+            const addpurchase = req.body
+            const result = await Allpurchase.insertOne(addpurchase);
+            res.send({ result: 'Added task Successfully' })
+        })
+        app.get('/purchase', async (req, res) => {
+            const query = {}
+            const cursor = Allpurchase.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get('/purchase/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await Allpurchase.findOne(query)
+            res.send(result)
+        })
+
+        app.delete('/purchase/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await Allpurchase.deleteOne(query)
             res.send(result)
         })
 
@@ -76,6 +107,31 @@ async function run() {
             const result = await newTaskCollection.find(query).toArray()
             res.send(result)
         });
+        //Task
+        app.post('/newTask', async (req, res) => {
+            const addTask = req.body
+            const result = await allTask.insertOne(addTask);
+            res.send({ result: 'Added task Successfully' })
+        })
+        app.get('/taskAll', async (req, res) => {
+            const query = {}
+            const cursor = allTask.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+        app.get('/taskAll/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await allTask.findOne(query)
+            res.send(result)
+        })
+        app.delete('/taskAll/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await allTask.deleteOne(query)
+            res.send(result)
+        })
+
 
     }
     finally {
