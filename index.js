127e64a1b1f736571da6dce85c9d598edf6d4fb9
rana135 <rana286090@gmail.com>
2023-03-04 01:27:59 +0600

    mongodb uri change

--- index.js
+++ index.js
@@ -30,8 +30,7 @@ function verifyJWT(req, res, next) {
 }
 
 
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hv3djnv.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.huhu6.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 //console.log(uri);
 async function run() {
@@ -474,5 +473,5 @@ async function run() {
 run().catch(console.dir);
 
 app.listen(port, () => {
-    console.log(`Show Here ${port}`)
+    console.log(`Hello From Hexatech Server ${port}`)
 })
